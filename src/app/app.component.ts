89b6fc72ed63b5a2f18c36dae769dd0e3ceff69a
Jiya Shah <jiyajshah25@gmail.com>
2024-09-16 15:32:40 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from './Shared/Modules/user';
+import {JsonPipe,NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,JsonPipe,NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Jiya_hahAnguarAssignments';
+  title = 'Jiya_shahAnguarAssignments';
   fname: string = 'Jiya';
   lname: string ='Shah';
 
+  user1: User = {id:1,name:'Jiya Shah',email:'jiyajshah25@gmail.com',contacts:265546545,city:'windsor'};
 
 }
