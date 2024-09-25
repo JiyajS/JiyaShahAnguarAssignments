1614686ad761c19068eadd76ce96b807dd3a6dd6
Jiya Shah <jiyajshah25@gmail.com>
2024-09-24 22:17:18 -0400

    changes

--- src/app/user-information-list/user-information-list.component.ts
+++ src/app/user-information-list/user-information-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
+import { Component, Input, input } from '@angular/core';
+import { User } from '../Shared/Modules/user';
 @Component({
   selector: 'app-user-information-list',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './user-information-list.component.html',
   styleUrl: './user-information-list.component.css'
 })
 export class UserInformationListComponent {
-
+  @Input() userList?:User;
 }
--- src/app/user-information/user-information.component.ts
+++ src/app/user-information/user-information.component.ts
@@ -12,6 +12,22 @@ import {User} from "../Shared/Modules/user";
   styleUrl: './user-information.component.css'
 })
 export class UserInformationComponent {
+  fname: string = 'Jiya';
+  lname: string ='Shah';
+
+  users: User[] = [
+    { id: 1, name: 'Jiya Shah', email: 'jiyajshah25@gmail.com', contacts: 456456, Admin:true },
+    { id: 2, name: 'Javal Patel', email: 'javalptl@gmail.com', contacts: 54645, Admin: true },
+    { id: 3, name: 'Manasvi Patel', email: 'm@gmail.com', contacts: 123123, Admin: false },
+    { id: 4, name: 'Sakshi', email: 's@gmail.com', contacts: 5645484, Admin:false },
+    { id: 5, name: 'Parimal', email: 'p@gmail.com', contacts: 8528787, Admin:false },
+    { id: 6, name: 'Chaula', email: 'c@gmail.com', contacts: 6565466, Admin:true }
+  ];
+
+  Login(user:User):void {
+    user.Admin = !user.Admin;
+  }
+
 
 
 
