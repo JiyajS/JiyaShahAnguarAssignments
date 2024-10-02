3119c4abf4a44b30f2d490d8bfe5b67f734851bb
Jiya Shah <jiyajshah25@gmail.com>
2024-10-02 12:56:14 -0400

    change4

--- src/app/user-information/user-information.component.ts
+++ src/app/user-information/user-information.component.ts
@@ -3,6 +3,7 @@ import {NgForOf} from "@angular/common";
 import {Operator} from "../Shared/Modules/operator";
 import {UserInformationListComponent} from "../user-information-list/user-information-list.component";
 import {operate} from "rxjs/internal/util/lift";
+import {OnlineBankingService} from "../online-banking.service";
 
 @Component({
   selector: 'app-user-information',
@@ -19,14 +20,16 @@ export class UserInformationComponent {
   lname: string ='Shah';
 
   login: Operator[] = [
-    { id: 1, name: 'Jiya Shah', email: 'jiyajshah25@gmail.com', contacts: 456456, Admin:true },
-    { id: 2, name: 'Javal Patel', email: 'javalptl@gmail.com', contacts: 54645, Admin: true },
-    { id: 3, name: 'Manasvi Patel', email: 'm@gmail.com', contacts: 123123, Admin: false },
-    { id: 4, name: 'Sakshi', email: 's@gmail.com', contacts: 5645484, Admin:false },
-    { id: 5, name: 'Parimal', email: 'p@gmail.com', contacts: 8528787, Admin:false },
-    { id: 6, name: 'Chaula', email: 'c@gmail.com', contacts: 6565466, Admin:true }
+    // { id: 1, name: 'Jiya Shah', email: 'jiyajshah25@gmail.com', contacts: 456456, Admin:true },
+    // { id: 2, name: 'Javal Patel', email: 'javalptl@gmail.com', contacts: 54645, Admin: true },
+    // { id: 3, name: 'Manasvi Patel', email: 'm@gmail.com', contacts: 123123, Admin: false },
+    // { id: 4, name: 'Sakshi', email: 's@gmail.com', contacts: 5645484, Admin:false },
+    // { id: 5, name: 'Parimal', email: 'p@gmail.com', contacts: 8528787, Admin:false },
+    // { id: 6, name: 'Chaula', email: 'c@gmail.com', contacts: 6565466, Admin:true }
   ];
-
+constructor(private OnlineBanking : OnlineBankingService ) {
+  this.login = this.OnlineBanking.getUser();
+}
 
 
 
