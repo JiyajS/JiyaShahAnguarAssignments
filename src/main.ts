c48119d2b74bb15185ac098457d184b0fefc2f88
Jiya Shah <jiyajshah25@gmail.com>
2024-10-07 19:09:32 -0400

    change8

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {UserInformationComponent} from "./app/user-information/user-information.component";
+import {provideRouter,Routes} from '@angular/router';
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+const routes: Routes = [
+  { path: 'user', component: UserInformationComponent },
+  { path: 'users/:Id', component: UserInformationComponent },
+  {path: 'modifyListItem', component: ModifyListItemComponent},
+  {path: 'pageNotFound', component: PageNotFoundComponent}
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
