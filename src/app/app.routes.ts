c0623647ee5089f765a560ba956a8fa42f38f5e0
Jiya Shah <w0835521@myscc.cs>
2024-11-01 09:21:08 -0400

    cs1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {ModifyListItemComponent} from "./modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'modify', component: ModifyListItemComponent },
+  { path: '**', component: PageNotFoundComponent } // Handle 404s
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
