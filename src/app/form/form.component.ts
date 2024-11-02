99e28330ecfa0899b88734f5e061ada3207f8cf5
Jiya Shah <w0835521@myscc.cs>
2024-11-01 23:48:21 -0400

    created branch

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}
