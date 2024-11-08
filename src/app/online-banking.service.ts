09f9576c5775a780684779c12351af15d01399ac
Jiya Shah <w0835521@myscc.cs>
2024-11-08 17:10:44 -0500

    edited service file with http

--- src/app/online-banking.service.ts
+++ src/app/online-banking.service.ts
@@ -1,66 +1,47 @@
 import { Injectable } from '@angular/core';
 import {login} from "./Shared/userChanges.data";
-import {Observable, of} from 'rxjs';
+import {Observable, of,catchError,throwError} from 'rxjs';
 // @ts-ignore
 import {Operator} from "../Shared/Modules/operator"
-import {operate} from "rxjs/internal/util/lift";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OnlineBankingService {
-
+private  apiUrl = 'api/login';
   private users: Operator[] = login;
 
-  constructor() {
-  }
-
-  getUser(): Observable<Operator[]> {
-    return of(login);
-  }
-
-  //Crud methods
-  // addUser(newUser: Operator): Observable<Operator[]> {
-  //   this.users.push(newUser);
-  //   return of(this.users);
-  // }
-
-  // updateUser(updateUser: Operator): Observable<Operator[]> {
-  //   const index = this.users.findIndex(user => user.id === updateUser.id);
-  //   if(index !== -1) {
-  //     this.users[index] = updateUser;
-  //   }
-  //   return of(this.users);
-  // }
-
-  updateUser(user: Operator): Observable<Operator> {
-    const index = this.users.findIndex(u => u.id === user.id);
-    if (index !== -1) {
-      this.users[index] = user;
-      console.log("User updated:", user);
+  constructor(private http: HttpClient) {}
+    getUser():Observable<Operator[]>{
+      return this.http.get<Operator[]>(this.apiUrl).pipe(catchError(this.handleError));
     }
-    return of(user); // Return an observable with the updated user
+
+  updateUser(user: Operator): Observable<Operator> | undefined{
+    const url = `${this.apiUrl}/${user.id}`;
+   return this.http.put<Operator>(url, user).pipe(catchError(this.handleError));
   }
 
   addUser(user: Operator): Observable<Operator> {
-    this.users.push(user);
-    console.log("User added:", user);
-    return of(user); // Return an observable with the new user
+     user.id = this.generateNewId();
+     return this.http.post<Operator>(this.apiUrl, user).pipe(catchError(this.handleError));
   }
 
-
-
-  deleteUser(id: number): Observable<Operator[]> {
-    this.users = this.users.filter(user => user.id !== id);
-    return of(this.users);
+  deleteUser(id: number): Observable<{}> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url).pipe(catchError(this.handleError));
   }
 
-  getUserById(id: number): Observable<Operator | undefined> {
-    const user = this.users.find(user => user.id === id);
-    return of(user);
+  getUserById(id: number): Observable<Operator> {
+   return this.http.get<Operator>(`${this.apiUrl}/${'id'}`).pipe(catchError(this.handleError));
   }
   generateNewId(): number{
     return this.users.length > 0 ? Math.max(...this.users.map(user => user.id)) + 1 : 1;
+  }
+  private handleError(error: HttpErrorResponse){
+    console.error('API error:',error);
+    return throwError(()=> new Error('Server error, please try again'))
   }
 
 
