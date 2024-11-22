19f84f593254db1d5cd001e32a8a41fbefa530c7
Jiya Shah <w0835521@myscc.cs>
2024-11-22 14:14:19 -0500

    assignment9 done1

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -10,7 +10,7 @@ import {HoverHighlightDirective} from "../directives/hover-highlight.directive";
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [NgIf, FormsModule, ReactiveFormsModule, HighlightOnFocusDirective, HoverHighlightDirective],
+  imports: [FormsModule, ReactiveFormsModule, HoverHighlightDirective],
   templateUrl: './modify-list-item.component.html',
   styleUrls: ['./modify-list-item.component.css']
 })
